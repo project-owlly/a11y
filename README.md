7fea8154a591b8b2464b72033880dd546d385833
Sandro Scalco <sandro.scalco@liitu.ch>
2021-11-24 13:12:04 +0100

    feat first test

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # a11y
 Accessible democracy with owlly
+
+
+# Installed packages
+- npm install @ionic/angular@next
+// - npm install @ionic/core@next
+// - npm install @stencil/core@latest
+- ng add @angular/pwa
+
---
+++ src/app/pages/form/form.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FormPageRoutingModule } from './form-routing.module';
+
+import { FormPage } from './form.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FormPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [FormPage]
+})
+export class FormPageModule {}
